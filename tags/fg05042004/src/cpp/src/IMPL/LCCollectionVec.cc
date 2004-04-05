2d72845df5a52bc78d4a7e26eea7fed0fa4579af
nobody <nobody@b31de640-2299-40b3-9495-e86d5be2208d>
2004-04-05 13:37:42 +0000

    This commit was manufactured by cvs2svn to create tag 'fg05042004'.

---
+++ tags/fg05042004/src/cpp/src/IMPL/LCCollectionVec.cc
@@ -0,0 +1,107 @@
+
+#include "IMPL/LCCollectionVec.h"
+#include "EVENT/LCIO.h"
+#include <iostream>
+
+using namespace EVENT ;
+
+namespace IMPL {
+
+//     std::string _typeName ;
+//     int _flag ;
+
+
+LCCollectionVec::LCCollectionVec( const std::string& type ) :
+  _typeName( type ),
+  _flag(0){ 
+  //,
+  // _access(LCIO::UPDATE ) {
+
+}
+  
+// LCCollectionVec::LCCollectionVec( const EVENT::LCCollection& col ) :
+//   _typeName( col.getTypeName() ),
+//   _flag( col.getFlag() ),
+//   _access( col._access ) {
+
+//   // deep copy of all elements  - requires clone of original elements
+//   //
+//   int nElements = col.getNumberOfElements() ;
+
+//   for(int i=0; i< nElements ; i++){
+//     push_back(  col.getElementAt( i )->clone() ) ;
+//   }
+
+// }
+
+  // overwrite the default implementation
+  // set flag in all elements
+  void LCCollectionVec::setReadOnly(bool readOnly){
+
+    AccessChecked::setReadOnly(readOnly ) ;
+
+    LCObjectVec::const_iterator iter = begin() ;
+    while( iter != end() ){
+      AccessChecked* element = dynamic_cast<AccessChecked*>(*iter++) ;
+      if(element){
+	element->setReadOnly( readOnly ) ;
+      }
+    }
+  }
+
+LCCollectionVec::~LCCollectionVec() {
+
+  // delete all elements
+  LCObjectVec::const_iterator iter = begin() ;
+  while( iter != end() ){
+    delete *iter++ ;
+  }
+
+}
+
+
+int LCCollectionVec::getNumberOfElements() const{
+  return size() ;
+}
+
+
+const std::string & LCCollectionVec::getTypeName() const{
+  return _typeName ;
+}
+
+
+
+DATA::LCObject * LCCollectionVec::getElementAt(int index) const{
+  return this->operator[](index) ;
+}
+
+
+int LCCollectionVec::getFlag() const{
+  return _flag ;
+}
+
+
+void LCCollectionVec::setFlag(int flag){
+  _flag  = flag ;
+}
+
+
+  void LCCollectionVec::addElement(DATA::LCObject * obj) throw (ReadOnlyException){
+    
+    //    if(_access != LCIO::UPDATE )
+    //  throw ReadOnlyException("LCCollectionVec::addElement:  event is read only") ;
+    checkAccess("LCCollectionVec::addElement") ;
+    this->push_back( obj ) ; 
+  }
+
+  void LCCollectionVec::removeElementAt(int i) throw (EVENT::ReadOnlyException){
+    
+    //    if(_access != LCIO::UPDATE )
+    //  throw ReadOnlyException("LCCollectionVec::addElement:  event is read only") ;
+    checkAccess("LCCollectionVec::removeElementAt") ;
+    this->erase( begin() + i ) ;
+
+  }
+
+}; // namespace IMPL
+
---
+++ tags/fg05042004/src/java/hep/lcio/implementation/sio/SIOIntVec.java
@@ -0,0 +1,48 @@
+package hep.lcio.implementation.sio;
+
+import hep.lcd.io.sio.SIOInputStream;
+import hep.lcd.io.sio.SIOOutputStream;
+
+import hep.lcio.data.LCIntVec;
+
+import hep.lcio.implementation.event.ILCIntVec;
+
+import java.io.IOException;
+
+
+/**
+ *
+ * @author Tony Johnson
+ * @version $Id: SIOIntVec.java,v 1.2 2003-09-15 21:44:32 tonyj Exp $
+ */
+class SIOIntVec extends ILCIntVec
+{
+   SIOIntVec(SIOInputStream in, SIOEvent owner) throws IOException
+   {
+      setParent(owner);
+      size = in.readInt();
+      data = new int[size];
+      for (int i = 0; i < size; i++)
+         data[i] = in.readInt();
+   }
+
+   static void write(LCIntVec vec, SIOOutputStream out) throws IOException
+   {
+      if (vec instanceof SIOIntVec)
+         ((SIOIntVec) vec).write(out);
+      else
+      {
+         int[] data = vec.toArray();
+         out.writeInt(data.length);
+         for (int i = 0; i < data.length; i++)
+            out.writeInt(data[i]);
+      }
+   }
+
+   private void write(SIOOutputStream out) throws IOException
+   {
+      out.writeInt(size);
+      for (int i = 0; i < size; i++)
+         out.writeInt(data[i]);
+   }
+}
