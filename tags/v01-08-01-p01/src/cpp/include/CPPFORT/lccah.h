a2960b002c79cdd1d8553379e99bc3ab354acddc
nobody <nobody@b31de640-2299-40b3-9495-e86d5be2208d>
2007-02-09 11:37:02 +0000

    This commit was manufactured by cvs2svn to create tag 'v01-08-01-p01'.

---
+++ tags/v01-08-01-p01/src/cpp/include/CPPFORT/lccah.h
@@ -0,0 +1,56 @@
+/**Header file for the f77-wrapper functions of the CalorimeterHitImpl class.
+ * 
+ * @author F. Gaede
+ * @version Oct 11, 2003
+ */
+
+#include "cfortran.h"
+#include "cpointer.h"
+
+// Warning: dont use "_" in function names as this causes two many
+// trailing underscores on Linux
+
+PTRTYPE lccahcreate() ;
+int lccahdelete( PTRTYPE simcalhit ) ;
+int lccahid( PTRTYPE simcalhit ) ;
+
+int lccahgetcellid0( PTRTYPE simcalhit )  ;
+int lccahgetcellid1( PTRTYPE simcalhit )  ;
+float lccahgetenergy( PTRTYPE simcalhit )  ;
+float lccahgettime( PTRTYPE simcalhit )  ;
+int lccahgetposition( PTRTYPE simcalhit, float * )  ;
+int   lccahgettype( PTRTYPE simcalhit ) ;
+PTRTYPE lccahgetrawhit( PTRTYPE simcalhit ) ;
+
+int lccahsetcellid0( PTRTYPE simcalhit, int id0) ;
+int lccahsetcellid1( PTRTYPE simcalhit, int id1) ;
+int lccahsetenergy( PTRTYPE simcalhit, float en) ;
+int lccahsettime( PTRTYPE simcalhit, float time) ;
+int lccahsetposition( PTRTYPE simcalhit, float pos[3])  ;
+int lccahsettype( PTRTYPE simcalhit, int type ) ;
+int lccahsetrawhit( PTRTYPE simcalhit, PTRTYPE rawHit ) ;
+
+// now the fortran wrappers from cfortran.h
+extern "C"{
+FCALLSCFUN0(CFORTRANPNTR, lccahcreate,LCCAHCREATE, lccahcreate ) 
+FCALLSCFUN1(INT, lccahdelete, LCCAHDELETE, lccahdelete, CFORTRANPNTR ) 
+FCALLSCFUN1(INT, lccahid, LCCAHID, lccahid, CFORTRANPNTR ) 
+
+FCALLSCFUN1(INT, lccahgetcellid0, LCCAHGETCELLID0, lccahgetcellid0, CFORTRANPNTR ) 
+FCALLSCFUN1(INT, lccahgetcellid1, LCCAHGETCELLID1, lccahgetcellid1, CFORTRANPNTR ) 
+FCALLSCFUN1(FLOAT, lccahgetenergy, LCCAHGETENERGY, lccahgetenergy, CFORTRANPNTR ) 
+FCALLSCFUN1(FLOAT, lccahgettime, LCCAHGETTIME, lccahgettime, CFORTRANPNTR )
+FCALLSCFUN2(INT, lccahgetposition,LCCAHGETPOSITION,lccahgetposition, CFORTRANPNTR, FLOATV ) 
+FCALLSCFUN1(INT, lccahgettype, LCCAHGETTYPE, lccahgettype, CFORTRANPNTR )
+
+FCALLSCFUN1(CFORTRANPNTR, lccahgetrawhit, LCCAHGETRAWHIT, lccahgetrawhit, CFORTRANPNTR ) 
+
+FCALLSCFUN2(INT, lccahsetcellid0,LCCAHSETCELLID0,lccahsetcellid0, CFORTRANPNTR, INT ) 
+FCALLSCFUN2(INT, lccahsetcellid1,LCCAHSETCELLID1,lccahsetcellid1, CFORTRANPNTR, INT ) 
+FCALLSCFUN2(INT, lccahsetenergy,LCCAHSETENERGY,lccahsetenergy, CFORTRANPNTR, FLOAT ) 
+FCALLSCFUN2(INT, lccahsettime,LCCAHSETTIME,lccahsettime, CFORTRANPNTR, FLOAT ) 
+FCALLSCFUN2(INT, lccahsetposition,LCCAHSETPOSITION,lccahsetposition, CFORTRANPNTR, FLOATV ) 
+FCALLSCFUN2(INT, lccahsettype, LCCAHSETTYPE, lccahsettype, CFORTRANPNTR, INT ) 
+FCALLSCFUN2(INT, lccahsetrawhit, LCCAHSETRAWHIT, lccahsetrawhit, CFORTRANPNTR, INT ) 
+}
+
