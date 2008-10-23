8c4e0eae9bb81c30f2d7519f458debb76036a52a
nobody <nobody@b31de640-2299-40b3-9495-e86d5be2208d>
2008-10-23 19:16:43 +0000

    This commit was manufactured by cvs2svn to create tag 'v01-10-02-pre'.

---
+++ tags/v01-10-02-pre/bin/runrandomtests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+LOCALCLASSPATH=$LCIO/lib/lcio.jar:$LCIO/tools/sio.jar
+LOCALCLASSPATH=$LOCALCLASSPATH:$LCIO/tools/freehep-sio-2.0.jar:$LCIO/tools/freehep-xdr-2.0.3.jar
+
+case "`uname`" in
+  CYGWIN*)
+    # For Cygwin, switch paths to Windows format before running java
+    LOCALCLASSPATH=`cygpath --path --windows "$LOCALCLASSPATH"`
+    LCIO=`cygpath --path --windows "$LCIO"`
+  ;;
+esac
+
+java -cp $LOCALCLASSPATH hep.lcio.test.RandomEventTest
+java -cp $LOCALCLASSPATH hep.lcio.test.CreateRandomEvent rand_java.slcio
+$LCIO/bin/copyfix rand_java.slcio rand_cpp.slcio
+java -cp $LOCALCLASSPATH hep.lcio.test.CompareEvents rand_java.slcio rand_cpp.slcio
