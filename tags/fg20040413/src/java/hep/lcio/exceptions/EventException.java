8e8ca33ea16b4c192543b236f7466f17ff3d6c47
gaede <gaede@b31de640-2299-40b3-9495-e86d5be2208d>
2011-03-28 09:11:37 +0000

    no longer needed

--- tags/fg20040413/src/java/hep/lcio/exceptions/EventException.java
+++
@@ -1,9 +0,0 @@
-package hep.lcio.exceptions;
-
-
-/**
- * An exception thrown when adding a item which already exists to an event
- * @author Tony Johnson
- * @version $Id: EventException.java,v 1.2 2003-09-15 21:44:31 tonyj Exp $
- */
-public class EventException extends RuntimeException {}
