6afaed0c7be3f7f7b219806b4a5ea8a461dcffbb
gaede <gaede@b31de640-2299-40b3-9495-e86d5be2208d>
2013-06-07 14:29:05 +0000

    - rm obsolete v02-04-pre02

--- branches/v02-04-pre02/src/python/pyLCIO/__init__.py
+++
@@ -1,7 +0,0 @@
-from base.SetupLcioDictionary import setupLcioDictionary
-setupLcioDictionary()
-
-from base.DecorateLcioClasses import decorateLcioClasses
-decorateLcioClasses()
-
-from ROOT import EVENT, IMPL, IO, IOIMPL, UTIL
