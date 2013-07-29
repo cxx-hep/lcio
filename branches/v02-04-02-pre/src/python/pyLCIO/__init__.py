7be354e6406e9bca53bcbda29d41aa9a1613a4fb
gaede <gaede@b31de640-2299-40b3-9495-e86d5be2208d>
2013-07-29 16:34:57 +0000

    - create pre-release v02-04-02-pre

---
+++ branches/v02-04-02-pre/src/python/pyLCIO/__init__.py
@@ -0,0 +1,7 @@
+from base.SetupLcioDictionary import setupLcioDictionary
+setupLcioDictionary()
+
+from base.DecorateLcioClasses import decorateLcioClasses
+decorateLcioClasses()
+
+from ROOT import EVENT, IMPL, IO, IOIMPL, UTIL
